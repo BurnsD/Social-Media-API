fbeab8641cc33c52b4e494736769e40ee766c80f
BurnsD <Dustinburnsactech@gmail.com>
2022-03-26 11:47:36 -0500

    user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,8 +1,9 @@
+const { createSecureServer } = require('http2');
 const { Thought, User } = require('../models')
 
 const userController = {
     // Get all Users
-    getAllUser(req,res) {
+    getAllUser(req, res) {
         User.find({})
             .select('-__v')
             .sort({ _id: -1 })
@@ -12,6 +13,43 @@ const userController = {
             });
     },
     // Get 1 User by ID
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+    // Create User
+    createSecureServer({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    },
+    // Update by ID
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'This User does not exist' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+    // Delete
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    }
+};
 
-
-}
+module.exports = userController;
